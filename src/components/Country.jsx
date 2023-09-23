5347cd1752f36534f36beb2517c7c9bee47bffa8
mohosin2126 <mohosin212@gmail.com>
2023-09-23 20:55:34 +0800

    added button style and fixed card design

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -15,17 +15,17 @@ const Country = ({country ,handleVisitedCountry}) => {
 
     return (
         <div >
-            <div className="border-2
-        border-indigo-500/50 m-2 p-1  rounded-xl w-96 ">
-            <h1 className="text-center font-bold text-xl">{name.common}</h1>
-            <img  className="w-80 h-50 mx-auto mt-2  " src={flags.png} alt="" />
-            <h3 className="text-center">Capital: {capital}</h3>
-            <h3 className="text-center ">Region: {region}</h3>
-            <h5 className="text-center">Population: {population}</h5>
-   <h5 className="text-center">{cca3}</h5>
+            <div className="card shadow-xl border-2
+        border-indigo-500/50 m-2 p-1  rounded-xl w-96 h-96 ">
+            <h1 className="text-center font-bold text-2xl">{name.common}</h1>
+            <img  className="w-80 h-40 mx-auto mt-2  " src={flags.png} alt="" />
+            <h3 className="text-center mt-2 font-semibold  text-xl">Capital: {capital}</h3>
+            <h3 className="text-center  font-semibold mt-1">Region: {region}</h3>
+            <h5 className="text-center font-semibold ">Population: {population}</h5>
+   <h5 className="text-center font-semibold">{cca3}</h5>
    <div className="flex ">
-   <button onClick={handleClick} className="flex btn  mx-auto btn-outline btn-success btn-xs">{ !visited ? "going":"visited"}</button>
-   <button onClick={passWithParams} className="flex btn  mx-auto btn-outline btn-success btn-xs">Mark as visited</button>
+   <button onClick={handleClick} className="flex btn  mx-auto btn-outline btn-ghost text-red-500  btn-xs">{ !visited ? "going":"visited"}</button>
+   <button onClick={passWithParams} className="flex btn  mx-auto text-red-500 btn-outline btn-success btn-xs">Mark as visited</button>
    </div>
  
         </div>
