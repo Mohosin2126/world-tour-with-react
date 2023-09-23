298f9f58fb0173d6fe3da247e0056c2737a249ea
mohosin2126 <mohosin212@gmail.com>
2023-09-23 17:22:22 +0800

    added few css style

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,11 +1,16 @@
 const Country = ({country}) => {
-   const {name,flags,capital}=country
+   const {name,flags,capital,region,languages,population}=country;
     return (
-        <div className="border-2
-        border-indigo-500/50 m-4 p-4 rounded-4 ">
+        <div >
+            <div className="border-2
+        border-indigo-500/50 m-4 p-4 rounded-4 h-96 ">
             <h1 className="text-center font-bold text-xl">{name.common}</h1>
-            <img  className="w-80 mx-auto m-4 p-4 " src={flags.png} alt="" />
-            <h3 className="text-center">{capital}</h3>
+            <img  className="w-80 h-62 mx-auto m-2 p-2 " src={flags.png} alt="" />
+            <h3 className="text-center">Capital: {capital}</h3>
+            <h3 className="text-center ">Region: {region}</h3>
+            <h5 className="text-center">Population: {population}</h5>
+
+        </div>
         </div>
     );
 };
--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -15,13 +15,16 @@ useEffect(()=>{
 
 
     return (
+      
         <div>
            <h1 className="text-center font-bold "> Name: {countries.length}</h1>
 
-         {
+<div className="lg:grid lg:grid-cols-3">
+  
+{
           countries.map(country => <Country key={country.cca3} country={country}></Country>)
          }
-
+</div>
         </div>
     );
 };
