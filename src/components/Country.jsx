822566583e0cfcf72f8179cbb4b57d31ff220c24
mohosin2126 <mohosin212@gmail.com>
2023-09-23 17:47:53 +0800

    added visited btn

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
+
 const Country = ({country}) => {
-   const {name,flags,capital,region,languages,population}=country;
+   const {name,flags,capital,region,languages,population,cca3}=country;
+   const [visited,setVisited]=useState(false)
+   const handleClick=()=>{
+    setVisited(!visited)
+   }
     return (
         <div >
             <div className="border-2
-        border-indigo-500/50 m-4 p-4 rounded-4 h-96 ">
+        border-indigo-500/50 m-2 p-4 rounded-xl h-96 ">
             <h1 className="text-center font-bold text-xl">{name.common}</h1>
             <img  className="w-80 h-62 mx-auto m-2 p-2 " src={flags.png} alt="" />
             <h3 className="text-center">Capital: {capital}</h3>
             <h3 className="text-center ">Region: {region}</h3>
             <h5 className="text-center">Population: {population}</h5>
-
+   <h5 className="text-center">{cca3}</h5>
+   <button onClick={handleClick} className="flex btn  mx-auto btn-outline btn-success btn-xs">{ !visited ? "going":"visited"}</button>
+ 
         </div>
         </div>
     );
